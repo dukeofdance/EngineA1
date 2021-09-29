29b2b8646f147fc710422988c04764da78e25716
Ehren <ehren.chan@ontariotechu.net>
2021-09-29 12:07:07 -0400

    Scene switching

--- Assets/Scripts/MenuNavigator.cs
+++ Assets/Scripts/MenuNavigator.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuNavigator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool levelFailed = false;
+    bool levelEnd = false;
+    bool mainMenu = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            mainMenu = true;
+        }
+
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            levelFailed = true;
+        }
+
+        if (SceneManager.GetActiveScene().name == "LevelEnd")
+        {
+            levelEnd = true;
+        }
+
+        if (mainMenu && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
+        if (levelEnd && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
+        if (levelFailed && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+
     }
 }
